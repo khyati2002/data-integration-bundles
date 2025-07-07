d99d15d5251c160ada338222e8ed92999dcc9691
jaskirat <jaskirat.singh@salescode.ai>
2025-07-07 11:18:54 +0530

    Update KgbplStockTransformer.java

--- src/main/java/com/applicate/kgbpl/transformer/KgbplStockTransformer.java
+++ src/main/java/com/applicate/kgbpl/transformer/KgbplStockTransformer.java
@@ -44,9 +44,19 @@ public class KgbplStockTransformer extends AbstractTransformer<Map<String, Objec
 
         String caseQty = getValue(inputMap, "AvailPhysical");
 
+        // ✅ Determine supplierId from dataAreaId
+        String supplierId = "";
+        if ("kbpl".equalsIgnoreCase(dataAreaId)) {
+            supplierId = "KBLCompanySupplier";
+        } else if ("kgpl".equalsIgnoreCase(dataAreaId)) {
+            supplierId = "KGPLCompanySupplier";
+        } else if ("eafp".equalsIgnoreCase(dataAreaId)) {
+            supplierId = "EnrichCompanySupplier";
+        }
+
         responseMap.put("skuCode", skuCode);
         responseMap.put("warehouseId", warehouseId);
-        responseMap.put("supplier", warehouseId);
+        responseMap.put("supplierId", warehouseId);
         responseMap.put("batchId", "unassigned");
         responseMap.put("caseQty", caseQty);
         responseMap.put("pieceQty", 0);
@@ -55,7 +65,7 @@ public class KgbplStockTransformer extends AbstractTransformer<Map<String, Objec
         // mfgDate from prodDate
         if (inputMap.get("prodDate") != null) {
             String prodDateStr = inputMap.get("prodDate").toString();
-//            responseMap.put("mfgDate", prodDateStr);
+            responseMap.put("mfgDate", prodDateStr);
 
             // shelfLife = expDate - prodDate
             if (inputMap.get("expDate") != null) {
