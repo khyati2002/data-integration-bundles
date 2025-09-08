cf0a3bf61ca3f97d703173de03adba6d949a6be4
mrinal-sahai-dev <mrinal.sahai@salescode.ai>
2025-09-08 22:52:00 +0530

    Update UserEnrichmentItcl.java

--- src/main/java/com/applicate/unnati/enrichment/UserEnrichmentItcl.java
+++ src/main/java/com/applicate/unnati/enrichment/UserEnrichmentItcl.java
@@ -6,7 +6,7 @@ import com.salescode.dim.etl.EnrichmentResult;
 import com.salescode.dim.etl.OperationResult;
 import com.salescode.dim.etl.enrichment.AbstractEnrichment;
 
-public class UserEnrichmentITCL extends AbstractEnrichment<CommonDataModel> {
+public class UserEnrichmentItcl extends AbstractEnrichment<CommonDataModel> {
 
     public EnrichmentResult apply(CommonDataModel commonDataModel) {
         return new OperationResult.StepResult(OperationResult.Status.OK, "Data enriched successfully");
