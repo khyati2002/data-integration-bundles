befd640db905a174086481dd335fc049f230481c
mrinal-sahai-dev <mrinal.sahai@salescode.ai>
2025-09-08 15:46:24 +0530

    Update OutletDetailsTransformer.java

--- src/main/java/com/applicate/unnati/transformer/OutletDetailsTransformer.java
+++ src/main/java/com/applicate/unnati/transformer/OutletDetailsTransformer.java
@@ -33,6 +33,9 @@ public class OutletDetailsTransformer extends AbstractTransformer<Map<String, Ob
         result.put("lob", getString(inputMap, "lob"));
         result.put("modifiedBy", getString(inputMap, "modifiedBy"));
         result.put("version", getInteger(inputMap, "version"));
+        result.put("userName", getString(inputMap, "userName"));
+
+
 
 
         result.put("address", getString(inputMap, "address"));
