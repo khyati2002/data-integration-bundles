ea2a96a8cbf87c6aa9700d071494edd99dd15d8b
mrinal-sahai-dev <mrinal.sahai@salescode.ai>
2025-09-15 16:09:13 +0530

    Update GenericEntityLinkGenerationEnrichmentITCL.java

--- src/main/java/com/applicate/unnati/enrichment/GenericEntityLinkGenerationEnrichmentITCL.java
+++ src/main/java/com/applicate/unnati/enrichment/GenericEntityLinkGenerationEnrichmentITCL.java
@@ -6,8 +6,7 @@ import com.salescode.dim.etl.EnrichmentResult;
 import com.salescode.dim.etl.OperationResult;
 import com.salescode.dim.etl.enrichment.AbstractEnrichment;
 
-public class GenericEnrichment extends AbstractEnrichment<CommonDataModel> {
-
+public class GenericEntityLinkGenerationEnrichmentITCL extends AbstractEnrichment<CommonDataModel> {
     public EnrichmentResult apply(CommonDataModel commonDataModel) {
         return new OperationResult.StepResult(OperationResult.Status.OK, "Data enriched successfully");
     }
