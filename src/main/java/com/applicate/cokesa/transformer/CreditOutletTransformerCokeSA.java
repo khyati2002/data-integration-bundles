00ec2d5453de3b60aa648cd255616627b3413f6f
dibyasundar <dibya.sundar@salescode.ai>
2025-10-30 17:05:04 +0530

    update credit outlet transformer

--- src/main/java/com/applicate/cokesa/transformer/CreditOutletTransformerCokeSA.java
+++ src/main/java/com/applicate/cokesa/transformer/CreditOutletTransformerCokeSA.java
@@ -19,7 +19,7 @@ public class CreditOutletTransformerCokeSA extends AbstractTransformer<Map<Strin
     private Map<String, Object> createResponse(Map <String, Object> inputMap){
         Map<String, Object> response = new HashMap<>();
         if(NullUtils.isNull(inputMap.get("OM02_OUTNUM"))) throw new DataTransformationService.TransformationException("OutletCode cannot be null");
-        if(((Number) inputMap.get("OM02_CRDDAYCOD")).intValue()!=1 ||((Number) inputMap.get("OM02_CRDDAYCOD")).intValue()!=2) throw new DataTransformationService.TransformationException("Credit day code should be 1 or 2");
+        if(Integer.parseInt(inputMap.get("OM02_CRDDAYCOD").toString())!=1 && Integer.parseInt(inputMap.get("OM02_CRDDAYCOD").toString())!=2) throw new DataTransformationService.TransformationException("Credit day code should be 1 or 2");
         response.put("outletCode", inputMap.get("OM02_OUTNUM").toString().replaceAll("\\.0$", ""));
         response.put("baseCreditLimit", NullUtils.isNotNull(inputMap.get("OM02_CRDLIM"))?inputMap.get("OM02_CRDLIM").toString():null);
         response.put("creditDays", (NullUtils.isNotNull(inputMap.get("OM02_CRDDAY")) && ((Number)inputMap.get("OM02_CRDDAY")).intValue()>0)?inputMap.get("OM02_CRDDAY").toString():0);
