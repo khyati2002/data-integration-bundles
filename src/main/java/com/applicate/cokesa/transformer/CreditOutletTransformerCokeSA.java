bf9ce029c2c5f78fc96102032cc004e0fe210e3b
dibyasundar <dibya.sundar@salescode.ai>
2025-11-04 14:13:46 +0530

    update

--- src/main/java/com/applicate/cokesa/transformer/CreditOutletTransformerCokeSA.java
+++ src/main/java/com/applicate/cokesa/transformer/CreditOutletTransformerCokeSA.java
@@ -31,7 +31,7 @@ public class CreditOutletTransformerCokeSA extends AbstractTransformer<Map<Strin
                     :   0);
         }
         response.put("id", UUID.randomUUID().toString());
-        response.put("active_status", ActiveStatus.ACTIVE);
+        response.put("activeStatus", ActiveStatus.ACTIVE);
         return response;
     }
 }
