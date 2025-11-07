718dbf9dc59fcfde733d312333c53309cc93eaf7
samarth-scai <samarth.narayan@salescode.ai>
2025-11-07 12:24:28 +0530

    declaration of service moved inside apply method

--- src/main/java/com/applicate/validation/VistaarUserValidation.java
+++ src/main/java/com/applicate/validation/VistaarUserValidation.java
@@ -18,13 +18,14 @@ public class VistaarUserValidation extends AbstractValidationRule<User> {
     public static final String DISTRICT = "district";
     public static final String BRANCH = "branch";
     public static final String USER_HAS_INVALID_IMMEDIATE_PARENT = "User has invalid immediate parent";
-    final UserService userService = (UserService) ServiceLocator.lookup(User.class);
     static final String capitalCaseRegex = "(^[A-Z\\s]*$)";
     String regex = "(^[0-9]{10}$)";
     static final String capitalCaseBranchRegex = "^[A-Z]+[A-Z0-9!@#$&\\-.+]*$";
 
     @Override
     public OperationResult.StepResult apply(User user) {
+        UserService userService = (UserService) ServiceLocator.lookup(User.class);
+
         RegexValidation regexValidation = new RegexValidation();
         StringBuilder ruleResult = new StringBuilder();
 
