e229be8e7553461edd5ccf51266651677fefcb55
khyati12arora <khyati.arora@salescode.ai>
2025-11-10 17:44:05 +0530

    Revert "Update SalesDetailsTransformer.java"
    This reverts commit 81834cad985f811820fab91a99091da9eb8980cb.

--- src/main/java/com/applicate/unnati/transformer/SalesDetailsTransformer.java
+++ src/main/java/com/applicate/unnati/transformer/SalesDetailsTransformer.java
@@ -87,7 +87,7 @@ public class SalesDetailsTransformer extends AbstractTransformer<Map<String, Obj
         result.put("amount", getDouble(inputMap, "amount"));
         result.put("rowid", getInteger(inputMap, "rowid"));
         result.put("productCode", getString(inputMap, "productCode"));
-        result.put("productDetails", getProductDetails(inputMap));
+        result.put("productDetails",getProductDetailsList(inputMap));
 
         return result;
     }
@@ -214,16 +214,18 @@ public class SalesDetailsTransformer extends AbstractTransformer<Map<String, Obj
         }
     }
 
-    @SuppressWarnings("unchecked")
-    private Map<String, Object> getProductDetails(Map<String, Object> rawMap) {
+    private List<Map<String, Object>> getProductDetailsList(Map<String, Object> rawMap) {
         Object value = rawMap.get("productDetails");
-        if (!(value instanceof Map)) return null;
-
-        Map<String, Object> productMap = new LinkedHashMap<>((Map<String, Object>) value);
-        productMap.remove("productDetails"); // defensive
-        return productDetailsTransformer.transform(productMap);
+        if (value == null) return Collections.emptyList();
+        if (value instanceof List<?>) {
+            return ((List<?>) value).stream()
+                           .filter(Objects::nonNull)
+                           .map(item -> {
+                               return productDetailsTransformer.transform((Map<String, Object>) item);
+                           })
+                           .filter(obj -> true)
+                           .collect(Collectors.toList());
+        }
+        return Collections.emptyList();
     }
-
-
-
 }
