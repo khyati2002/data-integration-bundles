0fa48b78d6f6920624868802223019e59b73d6ab
yasir <yasir.sarfaraz@salescode.ai>
2025-11-10 12:18:43 +0530

    formatted

--- src/main/java/com/applicate/unnati/enrichment/ProductDetailsEnrichment.java
+++ src/main/java/com/applicate/unnati/enrichment/ProductDetailsEnrichment.java
@@ -7,27 +7,19 @@ import com.salescode.dim.etl.enrichment.AbstractEnrichment;
 import com.salescode.dim.jooq.impl.ProductDetails;
 
 /**
- * Flink-compatible ProductDetailsEnrichment
- * Adapted from Spring-based service to use jOOQ ProductDetails
- * 
  * Enriches product details with suggestion text and batch code
  * when these fields are empty or null.
- * 
- * @author System (Adapted for Flink)
- * @since Nov 2025
  */
 public class ProductDetailsEnrichment extends AbstractEnrichment<ProductDetails> {
 
-    @Override
-    public EnrichmentResult apply(ProductDetails cdm) {
-        if (StringUtils.isEmpty(cdm.getSuggestionText())) {
-            cdm.setSuggestionText(cdm.getSkuCode() + " " + cdm.getSkuDescription());
-        }
-
-        if (StringUtils.isEmpty(cdm.getBatchCode())) {
-            cdm.setBatchCode(cdm.getSkuCode());
-        }
-
-        return OperationResult.StepResult.OK;
-    }
+	@Override
+	public EnrichmentResult apply(ProductDetails cdm) {
+		if (StringUtils.isEmpty(cdm.getSuggestionText())) {
+			cdm.setSuggestionText(cdm.getSkuCode() + " " + cdm.getSkuDescription());
+		}
+		if (StringUtils.isEmpty(cdm.getBatchCode())) {
+			cdm.setBatchCode(cdm.getSkuCode());
+		}
+		return OperationResult.StepResult.OK;
+	}
 }
