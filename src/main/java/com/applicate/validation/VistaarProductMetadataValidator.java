882d83ab77071dbcfee6ae28316b7d11442b0eeb
samarth-scai <samarth.narayan@salescode.ai>
2025-11-12 16:57:00 +0530

    Revert "minor fixes"
    This reverts commit 8e1224c56dea3b06787da987bca0ac2133ab3fba.

--- src/main/java/com/applicate/validation/VistaarProductMetadataValidator.java
+++ src/main/java/com/applicate/validation/VistaarProductMetadataValidator.java
@@ -2,12 +2,12 @@ package com.applicate.validation;
 
 import com.applicate.services.channelkart.services.ProductDetailsService;
 import com.applicate.services.channelkart.services.ServiceLocator;
+import com.applicate.services.channelkart.services.UserDesignationService;
 import com.applicate.services.channelkart.services.UserService;
-import com.applicate.services.channelkart.services.UserdesignationService;
 import com.applicate.services.channelkart.validations.repository.RegexValidation;
 import com.salescode.dim.etl.OperationResult;
 import com.salescode.dim.jooq.impl.ProductDetails;
-import com.salescode.dim.jooq.impl.Userdesignation;
+import com.salescode.dim.jooq.impl.UserDesignation;
 import org.apache.commons.lang3.StringUtils;
 import com.salescode.dim.etl.validation.AbstractValidationRule;
 import com.salescode.dim.jooq.impl.ProductMetaData;
@@ -149,6 +149,7 @@ public class VistaarProductMetadataValidator extends AbstractValidationRule<Prod
 
 	private void validateSupplier(ProductMetaData cdm, List<String> errors) {
 		UserService userService = (UserService) ServiceLocator.lookup(User.class);
+		UserDesignationService userDesignationService = (UserDesignationService) ServiceLocator.lookup(UserDesignation.class);
 		if (cdm.getLoginid() == null) {
 			errors.add("supplier should not be null or empty");
 			return;
@@ -156,7 +157,7 @@ public class VistaarProductMetadataValidator extends AbstractValidationRule<Prod
 
 		User user = userService.findByLoginId(cdm.getLoginid());
 		if(user!=null){
-			Set<String> designations = UserdesignationService.findDesignations(getDslContext(), user.getLoginId());
+			Set<String> designations = userDesignationService.getDesignationsByLoginId(user.getLoginId());
 			user.setDesignation(designations);
 		}
 
