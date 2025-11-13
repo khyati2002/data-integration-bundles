487f94b53c32128a3fa5cd0f70d523bff24b0e0f
Aakash Patel <aakash.patel@salescode.ai>
2025-11-13 15:00:48 +0530

    Update OrdersTransformer.java

--- src/main/java/com/applicate/unnati/transformer/OrdersTransformer.java
+++ src/main/java/com/applicate/unnati/transformer/OrdersTransformer.java
@@ -45,7 +45,7 @@ public class OrdersTransformer extends AbstractTransformer<Map<String, Object>, 
             output.put("normalizedQuantity",0);
             output.put("initialNormalizedQuantity",0);
             output.put("normalizedVolume",0);
-            output.put("totalInitialAmount",0);
+            output.put("totalInitialAmt",0);
 
             // Map date fields
             output.put("deliveryDate", getFormattedDate(responseEnvelope, "deliveryDate"));
