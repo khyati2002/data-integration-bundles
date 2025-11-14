0d8b5cfee0d4accb40c30725fb189e96139bb3f5
Manasvi Gaur <manasvi.gaur@salescode.ai>
2025-11-14 10:35:33 +0530

    Update CokephDistributorMasterTransformer.java

--- src/main/java/com/applicate/cokeph/transformer/CokephDistributorMasterTransformer.java
+++ src/main/java/com/applicate/cokeph/transformer/CokephDistributorMasterTransformer.java
@@ -40,7 +40,11 @@ public class CokephDistributorMasterTransformer extends AbstractTransformer<Map<
         validateDesignation(user);
         finalTransformedObj.put("userAccountId", stringObjectMap.get("distributor_code").toString());
         finalTransformedObj.put("designation","supplier");
-        finalTransformedObj.put("immediateParent", "admin@applicate.in");
+        ObjectNode parentObj = mapper.createObjectNode();
+        parentObj.put("immediateParent", "admin@applicate.in");
+        finalTransformedObj.put("immediateParent", parentObj);
+
+//        finalTransformedObj.put("immediateParent", "admin@applicate.in");
         //adding supplier metadata
 
         ObjectNode supplierMetaData = mapper.createObjectNode();
