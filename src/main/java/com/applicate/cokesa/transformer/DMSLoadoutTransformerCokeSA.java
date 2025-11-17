0f3fbaaf837be3d6e6c5885b9dee3a4b0fae2b07
dibyasundar <dibya.sundar@salescode.ai>
2025-11-17 10:49:51 +0530

    added salesinfo

--- src/main/java/com/applicate/cokesa/transformer/DMSLoadoutTransformerCokeSA.java
+++ src/main/java/com/applicate/cokesa/transformer/DMSLoadoutTransformerCokeSA.java
@@ -6,6 +6,7 @@ import com.salescode.dim.jooq.generated.enums.*;
 import com.salescode.dim.jooq.generated.tables.pojos.DmsLoadout;
 import com.salescode.dim.jooq.impl.LoadoutDetails;
 import com.salescode.dim.jooq.impl.LoadoutItems;
+import com.salescode.dim.jooq.impl.SalesInfo;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -67,6 +68,8 @@ public class DMSLoadoutTransformerCokeSA  extends AbstractTransformer<Map<String
             loadoutDetails.setReturnPieceQty(0.0);
             loadoutDetails.setReturnOtherQty(0.0);
             loadoutDetails.setTotalAmount(BigDecimal.ZERO);
+            SalesInfo salesInfo=new SalesInfo(BigDecimal.ZERO,BigDecimal.ZERO,BigDecimal.ZERO,BigDecimal.ZERO,"OUT FOR DELIVERY");
+            loadoutDetails.setSalesInfo(salesInfo);
             loadoutDetailsList.add(loadoutDetails);
         }
         return loadoutDetailsList;
