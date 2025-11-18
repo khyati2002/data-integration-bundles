899c6d522e242926346c5b4f795b1ce238a264d5
Anish Raj <ranishraj10282@gmail.com>
2025-11-18 11:36:18 +0530

    Update UserTransformer.java

--- src/main/java/com/applicate/alsafi/transformer/UserTransformer.java
+++ src/main/java/com/applicate/alsafi/transformer/UserTransformer.java
@@ -2,6 +2,7 @@ package com.applicate.alsafi.transformer;
 
 import com.salescode.dim.etl.transformation.AbstractTransformer;
 
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,18 +11,30 @@ public class UserTransformer extends AbstractTransformer<Map<String,Object>, Map
     public Map<String, Object> transform(Map<String, Object> inputMap) {
         Map<String, Object> userMap = new HashMap<>();
         userMap.put("loginId", inputMap.get("loginId"));
+        userMap.put("userAccountId", inputMap.get("loginId"));
         userMap.put("mobile","0000000000");
         userMap.put("name", inputMap.get("name"));
         userMap.put("immediateParent", inputMap.get("parent"));
         userMap.put("designation", inputMap.get("designation"));
-        Map<String, Object> location = new HashMap<>();
-        location.put("country", "Saudi Arabia");
-        if(inputMap.containsKey("region")) location.put("test", "test");
-        userMap.put("locationHierarchy", location);
+        userMap.put("locationHierarchy", getLocationObject(inputMap));
         return userMap;
     }
-    
 
+    private Map<String, Object> getLocationObject(Map<String, Object> inputMap){
+        Map<String, Object> locationObject = new HashMap<>();
+        locationObject.put("country", "Saudi Arabia");
+        checkAndUpdate("region", inputMap, locationObject);
+        checkAndUpdate("branch", inputMap, locationObject);
+        checkAndUpdate("area", inputMap, locationObject);
+        checkAndUpdate("district", inputMap, locationObject);
+        return locationObject;
+    }
+
+    private void checkAndUpdate(String key, Map<String, Object> inputMap, Map<String, Object> locationObject){
+        if(inputMap.containsKey(key) && inputMap.get(key) != null){
+            locationObject.put(key, inputMap.get(key));
+        }
+    }
 
 
 }
