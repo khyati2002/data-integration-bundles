5f7ce37afcd799310da368701714227334920673
jaskirat <jaskirat.singh@salescode.ai>
2025-11-18 13:29:58 +0530

    Update CustomerEnrichmentKgbpl.java

--- src/main/java/com/applicate/kgbpl/enrichment/CustomerEnrichmentKgbpl.java
+++ src/main/java/com/applicate/kgbpl/enrichment/CustomerEnrichmentKgbpl.java
@@ -2,6 +2,7 @@ package com.applicate.kgbpl.enrichment;
 
 import com.applicate.services.channelkart.services.CategoryInfoService;
 import com.applicate.services.channelkart.services.ServiceLocator;
+import com.applicate.services.channelkart.services.UserService;
 import com.applicate.services.channelkart.utils.StringUtils;
 import com.salescode.dim.etl.OperationResult;
 import com.salescode.dim.etl.enrichment.AbstractEnrichment;
@@ -18,19 +19,18 @@ public class CustomerEnrichmentKgbpl extends AbstractEnrichment<OutletDetails> {
         CategoryInfoService categoryInfoService =
                 (CategoryInfoService) ServiceLocator.lookup(CategoryInfo.class);
 
+        UserService userService = (UserService) ServiceLocator.lookup(User.class);
 
         // ---------------------------
         // Area enrichment
         // ---------------------------
-        if (cdm.getLocation() != null && !StringUtils.isEmpty(cdm.getLocation().getArea())) {
-            List<CategoryInfo> areaList = categoryInfoService.findByCategoryCodeAndFeature(
-                    cdm.getLocation().getArea(), "area"
-            );
-            if (!areaList.isEmpty()) {
-                String areaValue = areaList.get(0).getCategoryValue();
-                if (!StringUtils.isEmpty(areaValue)) {
-                    cdm.getLocation().setArea(areaValue);
-                }
+        List<CategoryInfo> areaList = categoryInfoService.findByCategoryCodeAndFeature(
+                cdm.getLocation().getArea(), "area"
+        );
+        if (!areaList.isEmpty()) {
+            String areaValue = areaList.get(0).getCategoryValue();
+            if (!StringUtils.isEmpty(areaValue)) {
+                cdm.getLocation().setArea(areaValue);
             }
         }
 
@@ -122,7 +122,8 @@ public class CustomerEnrichmentKgbpl extends AbstractEnrichment<OutletDetails> {
                 cdm.setChannel(channelValue);
             }
         }
-// ---------------------------
+
+        // ---------------------------
         // AccountInfo enrichment block (commented)
         // ---------------------------
 //        AccountInfoService accountInfoService =
@@ -156,6 +157,7 @@ public class CustomerEnrichmentKgbpl extends AbstractEnrichment<OutletDetails> {
 //        if (isNewAccount || isUpdated) {
 //            accountInfoService.save(decrypted);
 //        }
+
         return new OperationResult.StepResult(OperationResult.Status.OK, "Customer enriched successfully");
     }
 }
