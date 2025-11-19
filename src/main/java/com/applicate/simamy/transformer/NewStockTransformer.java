4a5b204f5368ee0d7e4cf89bbcf6ca6dd9035aed
siddharth-2510 <siddharth.jaiswal@salescode.ai>
2025-11-19 13:57:17 +0530

    Update NewStockTransformer.java

--- src/main/java/com/applicate/simamy/transformer/NewStockTransformer.java
+++ src/main/java/com/applicate/simamy/transformer/NewStockTransformer.java
@@ -1,78 +1,68 @@
 package com.applicate.simamy.transformer;
 
-import com.applicate.services.channelkart.exceptions.TransformationException;
-import com.applicate.services.channelkart.models.MetaData;
-import com.applicate.services.channelkart.models.Stock;
-import com.applicate.services.channelkart.repository.StockRepository;
-import com.applicate.services.channelkart.services.MetaDataService;
-import com.applicate.services.channelkart.services.ProductDetailsService;
-import com.applicate.services.channelkart.services.SpringContext;
-import com.applicate.services.channelkart.services.StockService;
-import com.applicate.services.channelkart.transformers.AbstractTransformer;
+import com.applicate.services.channelkart.services.*;
 import com.applicate.services.channelkart.utils.JSONUtils;
-import org.apache.commons.lang3.ObjectUtils;
+import com.salescode.dim.etl.transformation.AbstractTransformer;
+import com.salescode.dim.etl.transformation.service.DataTransformationService;
+import com.salescode.dim.jooq.generated.tables.pojos.Metadata;
+import com.salescode.dim.jooq.generated.tables.pojos.Productdetails;
+import com.salescode.dim.jooq.generated.tables.pojos.Producttag;
+import com.salescode.dim.jooq.generated.tables.pojos.Stock;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.core.type.TypeReference;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
 
-import java.util.Date;
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class NewStockTransformer extends AbstractTransformer<Map<String, Object>, Map<String, Object>> {
-    final StockRepository stockRepository = SpringContext.getBean(StockRepository.class);
-    final StockService stockService = SpringContext.getBean(StockService.class);
-    final MetaDataService metaDataService = SpringContext.getBean(MetaDataService.class);
-    final ProductDetailsService productDetailsService = SpringContext.getBean(ProductDetailsService.class);
+
+    final StockService stockService = (StockService) ServiceLocator.lookup(Stock.class);
+    final MetaDataService metaDataService = (MetaDataService) ServiceLocator.lookup(Metadata.class);
+    final ProductDetailsService productDetailsService = (ProductDetailsService) ServiceLocator.lookup(Productdetails.class);
+
     static final String SKU_CODE = "skuCode";
     static final String SUPPLIER = "supplier";
     static final String AVAILABLE_STOCK = "qty";
 
     @Override
-    public Object transform(Map<String, Object> inputMap) {
-        MetaData migratedSuppliers =  metaDataService.fetchByValue("MigratedSuppliers","orderSubmit");
-        Map<String,String> supplierList = JSONUtils.toStringMap(migratedSuppliers.getDomainValues().get(0));
-        Map<String, Object> stockTransformed = new HashMap<>();
-        final String supplier = ObjectUtils.isEmpty(inputMap.get(SUPPLIER)) ? null : inputMap.get(SUPPLIER).toString();
-        final String skuCode = ObjectUtils.isEmpty(inputMap.get(SKU_CODE)) ? null : inputMap.get(SKU_CODE).toString();
-        final String qty = ObjectUtils.isEmpty(inputMap.get(AVAILABLE_STOCK)) ? null : inputMap.get(AVAILABLE_STOCK).toString();
-        if(productDetailsService.checkIfBatchCodeExists(skuCode)) {
-            Stock existingDetails = getExistingDetails(skuCode, supplier);
-            Date now = new Date();
-            if (supplier != null && qty != null && supplierList.containsKey(supplier.toString())) {
+    public Map<String, Object> transform(Map<String, Object> inputMap) {
+        Metadata migratedSuppliers = metaDataService.fetchByValue("MigratedSuppliers", "orderSubmit");
+        Map<String,String> supplierList = JSONUtils.getObjectMapper().convertValue(migratedSuppliers.getDomainValues().get(0), Map.class);
+
+        final String supplier = inputMap.get(SUPPLIER)!=null ? null : inputMap.get(SUPPLIER).toString();
+        final String skuCode = inputMap.get(SKU_CODE)!=null? null : inputMap.get(SKU_CODE).toString();
+        final String qty =inputMap.get(AVAILABLE_STOCK)!=null ? null : inputMap.get(AVAILABLE_STOCK).toString();
+
+        if (productDetailsService.checkIfBatchCodeExists(skuCode)) {
+            Stock existingDetails = stockService.findBySkuCodeAndSupplier(skuCode, supplier);
+
+            if (supplier != null && qty != null && supplierList.containsKey(supplier)) {
                 if (existingDetails == null) {
-                    Stock stock = new Stock();
-//            stockTransformed.put(SUPPLIER,supplier);
-//            stockTransformed.put(AVAILABLE_STOCK,qty);
-//            stockTransformed.put(SKU_CODE,skuCode);
-                    String id = skuCode + "-" + supplier + "-" + skuCode;
-                    stock.setId(id);
-                    stock.setSupplier(supplier);
-                    stock.setSkuCode(skuCode);
-                    stock.setQty(Float.parseFloat(qty));
-                    stock.setVersion(0);
-                    stock.setBatchCode(skuCode);
-                    stock.setBatchPrice(0.0);
-                    stock.setInitialStock(0);
-                    stock.setInitialQty(0);
-                    stock.setMinQty(0);
-                    stock.setPrice(0);
-                    stock.setStockValue(0);
-                    return stockService.save(stock);
+                    Stock stock = createStock(skuCode, supplier, qty);
+                    return (Map)stockService.save(stock);
                 } else {
-                    existingDetails.setQty(Float.parseFloat(qty));
-                    return existingDetails;
+                    existingDetails.setQty(Double.parseDouble(qty));
+                    return (Map)stockService.save(existingDetails);
                 }
             }
+        } else {
+            throw new DataTransformationService.TransformationException("SKUCode not present in ProductMaster");
         }
-        else{
-            throw new TransformationException("SKUCode not present in ProductMaster");
-        }
-        throw new TransformationException("Check Supplier in Migrated list or qty is null");
 
+        throw new DataTransformationService.TransformationException("Check Supplier in Migrated list or qty is null");
     }
 
-
-    private Stock getExistingDetails(String skuCode,String supplier) {
-        if(!(stockService.findBySkuCodeAndSupplier(skuCode,supplier)==null))
-            return stockService.findBySkuCodeAndSupplier(skuCode,supplier);
-        return null;
+    private static Stock createStock(String skuCode, String supplier, String qty) {
+        Stock stock = new Stock();
+        String id = skuCode + "-" + supplier + "-" + skuCode;
+        stock.setId(id);
+        stock.setSupplier(supplier);
+        stock.setSkuCode(skuCode);
+        stock.setQty(Double.parseDouble(qty));
+        stock.setVersion(0);
+        stock.setBatchCode(skuCode);
+        stock.setSkuCode(skuCode);
+        stock.setMinQty(0);
+        return stock;
     }
 }
