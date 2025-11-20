3d2520b3043e92b740579ff4573b0170a4524a30
Mihir0809 <mihir.bhasin@salescode.ai>
2025-11-20 15:43:29 +0530

    ActiveStatus Change

--- src/main/java/com/applicate/cokesa/transformer/DMSLoadoutTransformerCokeSA.java
+++ src/main/java/com/applicate/cokesa/transformer/DMSLoadoutTransformerCokeSA.java
@@ -49,7 +49,7 @@ public class DMSLoadoutTransformerCokeSA  extends AbstractTransformer<Map<String
         dmsLoadout.setPieceShortage(0.0);
         dmsLoadout.setOtherShortage(0.0);
         dmsLoadout.setShortageUpdated((byte) 0);
-        dmsLoadout.setActiveStatus(ActiveStatus.ACTIVE);
+        dmsLoadout.setActiveStatus(DmsLoadoutActiveStatus.ACTIVE);
         dmsLoadout.setSupplier(supplier);
         return dmsLoadout;
     }
@@ -86,7 +86,7 @@ public class DMSLoadoutTransformerCokeSA  extends AbstractTransformer<Map<String
             );
             loadoutDetails.setSalesInfo(salesInfoJson);
 
-            loadoutDetails.setActiveStatus(ActiveStatus.ACTIVE);
+            loadoutDetails.setActiveStatus(DmsLoadoutDetailsActiveStatus.ACTIVE);
             loadoutDetailsList.add(loadoutDetails);
         }
         return loadoutDetailsList;
@@ -112,7 +112,7 @@ public class DMSLoadoutTransformerCokeSA  extends AbstractTransformer<Map<String
                 loadoutItems.setLoadOutDetailsId(loadOutDetailsId);
             }
 
-            loadoutItems.setActiveStatus(ActiveStatus.ACTIVE);
+            loadoutItems.setActiveStatus(DmsLoadoutItemsActiveStatus.ACTIVE);
 
             loadoutItemsList.add(loadoutItems);
         }
