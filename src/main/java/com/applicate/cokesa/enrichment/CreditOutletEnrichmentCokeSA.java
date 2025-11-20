869729e184cb66758d7b36b094e3b5a175658767
dibyasundar <dibya.sundar@salescode.ai>
2025-11-20 11:25:28 +0530

    update creditoutlettransformercokesa

--- src/main/java/com/applicate/cokesa/enrichment/CreditOutletEnrichmentCokeSA.java
+++ src/main/java/com/applicate/cokesa/enrichment/CreditOutletEnrichmentCokeSA.java
@@ -1,10 +1,12 @@
 package com.applicate.cokesa.enrichment;
 
 import com.applicate.services.channelkart.models.enums.ActiveStatus;
+import com.applicate.services.channelkart.services.DeliveryPJPService;
 import com.applicate.services.channelkart.services.OutletDetailsService;
 import com.applicate.services.channelkart.services.ServiceLocator;
 import com.salescode.dim.etl.OperationResult;
 import com.salescode.dim.etl.enrichment.AbstractEnrichment;
+import com.salescode.dim.jooq.generated.tables.pojos.DeliveryPjp;
 import com.salescode.dim.jooq.impl.CreditOutlets;
 import com.salescode.dim.jooq.impl.OutletDetails;
 
@@ -13,8 +15,11 @@ public class CreditOutletEnrichmentCokeSA extends AbstractEnrichment<CreditOutle
     @Override
     public OperationResult.StepResult apply(CreditOutlets creditOutlets) {
         OutletDetailsService outletDetailsService=(OutletDetailsService) ServiceLocator.lookup(OutletDetails.class);
+        DeliveryPJPService deliveryPJPService=(DeliveryPJPService)  ServiceLocator.lookup(DeliveryPjp.class);
 
         String outletCode= creditOutlets.getOutletCode();
+
+        String salesRep=deliveryPJPService.getLoginIdByOutletcode(outletCode);
 
         String outletName=outletDetailsService.getOutletNameByOutletCode(outletCode);
 
@@ -23,6 +28,8 @@ public class CreditOutletEnrichmentCokeSA extends AbstractEnrichment<CreditOutle
         }
         creditOutlets.setOutletName(outletName);
         creditOutlets.setActiveStatus(ActiveStatus.ACTIVE);
+        creditOutlets.setCreatedBy(salesRep);
+        creditOutlets.setModifiedBy(salesRep);
         return new OperationResult.StepResult(OperationResult.Status.OK,"OutletName set Successfully to creditOutlets");
     }
 }
