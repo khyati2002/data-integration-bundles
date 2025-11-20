af43baaed6f71578c42e78c0042a3e7c1b5102c1
siddharth-2510 <siddharth.jaiswal@salescode.ai>
2025-11-20 10:42:33 +0530

    test commented

--- src/main/java/com/applicate/simamy/enrichment/TestE.java
+++ src/main/java/com/applicate/simamy/enrichment/TestE.java
@@ -1,13 +1,13 @@
-package com.applicate.simamy.enrichment;
-
-import com.applicate.services.channelkart.enrichments.AbstractEnrichment;
-import com.applicate.services.channelkart.enrichments.EnrichmentResult;
-import com.applicate.services.channelkart.models.User;
-
-public class TestE extends AbstractEnrichment<User> {
-
-  @Override
-  public EnrichmentResult apply(User user) {
-    return null;
-  }
-}
+//package com.applicate.simamy.enrichment;
+//
+//import com.applicate.services.channelkart.enrichments.AbstractEnrichment;
+//import com.applicate.services.channelkart.enrichments.EnrichmentResult;
+//import com.applicate.services.channelkart.models.User;
+//
+//public class TestE extends AbstractEnrichment<User> {
+//
+//  @Override
+//  public EnrichmentResult apply(User user) {
+//    return null;
+//  }
+//}
