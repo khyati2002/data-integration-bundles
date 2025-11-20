a8785a2ac9f0ef7162ef0f77436db632ea212dd3
Chetan <chetan.sethi@salescode.ai>
2025-11-20 13:59:07 +0530

    Update DepotUserEnrichment.java

--- src/main/java/com/applicate/cokesa/enrichment/DepotUserEnrichment.java
+++ src/main/java/com/applicate/cokesa/enrichment/DepotUserEnrichment.java
@@ -2,6 +2,7 @@ package com.applicate.cokesa.enrichment;
 
 import com.applicate.services.channelkart.services.ServiceLocator;
 import com.applicate.services.channelkart.services.UserService;
+import com.applicate.services.channelkart.utils.SecurityContextUtils;
 import com.salescode.dim.etl.OperationResult;
 import com.salescode.dim.etl.enrichment.AbstractEnrichment;
 import com.salescode.dim.jooq.impl.OutletDetails;
@@ -16,26 +17,28 @@ public class DepotUserEnrichment extends AbstractEnrichment<OutletDetails> {
     @Override
     public OperationResult.StepResult apply(OutletDetails outlet) {
 
-        UserService userService=(UserService) ServiceLocator.lookup(User.class);
-        User user = userService.findByLoginId(outlet.getLoginid());
-        if(user!=null) {
-            outlet.setUserName(user);
-            user.setDesignation(Set.of("Depot"));
-            user.setUserParents(List.of("admin@applicate.in"));
-            user.setLoginId(outlet.getOutletcode());
-            user.setUserAccountId(outlet.getOutletcode());
-            user.setActiveStatus(outlet.getActiveStatus());
-            user.setLocationHierarchy(outlet.getLocationHierarchy());
-            user.setLocationHierarchy(outlet.getLocation());
-            user.setMobile(outlet.getContactno());
-            user.setAddress(outlet.getAddress());
-            user.setName(StringUtils.isEmpty(outlet.getOutletName()) ? outlet.getOutletcode() : outlet.getOutletName());
-            outlet.setUserName(user);
-            userService.save(user);
-        }
-        else {
-            user = outlet.getUserName();
-            outlet.setUserName(user);
+        String authToken = SecurityContextUtils.getPrincipal();
+        if ("integration_user".equals(authToken)) {
+            UserService userService = (UserService) ServiceLocator.lookup(User.class);
+            User user = userService.findByLoginId(outlet.getLoginid());
+            if (user != null) {
+                outlet.setUserName(user);
+                user.setDesignation(Set.of("Depot"));
+                user.setUserParents(List.of("admin@applicate.in"));
+                user.setLoginId(outlet.getOutletcode());
+                user.setUserAccountId(outlet.getOutletcode());
+                user.setActiveStatus(outlet.getActiveStatus());
+                user.setLocationHierarchy(outlet.getLocationHierarchy());
+                user.setLocationHierarchy(outlet.getLocation());
+                user.setMobile(outlet.getContactno());
+                user.setAddress(outlet.getAddress());
+                user.setName(StringUtils.isEmpty(outlet.getOutletName()) ? outlet.getOutletcode() : outlet.getOutletName());
+                outlet.setUserName(user);
+                userService.save(user);
+            } else {
+                user = outlet.getUserName();
+                outlet.setUserName(user);
+            }
         }
         return new OperationResult.StepResult(OperationResult.Status.OK);
     }
