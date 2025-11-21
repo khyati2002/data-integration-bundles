b9fbd7279a5fae2a6d43998af0a1581234abedc5
himanshi-0710 <himanshi.hans@salescode.ai>
2025-11-21 15:06:18 +0530

    Update PJPenrichment.java

--- src/main/java/com/applicate/kbpl/enrichment/PJPenrichment.java
+++ src/main/java/com/applicate/kbpl/enrichment/PJPenrichment.java
@@ -3,16 +3,14 @@ package com.applicate.kbpl.enrichment;
 import com.applicate.services.channelkart.services.DeliveryPJPService;
 import com.applicate.services.channelkart.services.ServiceLocator;
 import com.applicate.services.channelkart.utils.NullUtils;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ArrayNode;
 import com.salescode.dim.etl.EnrichmentResult;
 import com.salescode.dim.etl.OperationResult;
 import com.salescode.dim.etl.enrichment.AbstractEnrichment;
 import com.salescode.dim.jooq.impl.DeliveryPjp;
 import org.apache.commons.lang3.ObjectUtils;
-import org.jooq.JSON;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -21,12 +19,11 @@ public class PJPenrichment extends AbstractEnrichment<DeliveryPjp> {
 
     DeliveryPJPService deliveryPJPService ;
 
-
     private final ObjectMapper objectMapper = new ObjectMapper();
     @Override
     public EnrichmentResult apply(DeliveryPjp cdm) {
         deliveryPJPService = (DeliveryPJPService) ServiceLocator.lookup(DeliveryPjp.class);
-        JSON dayandfreq = cdm.getDayAndFrequency();
+        JsonNode dayandfreq = cdm.getDayAndFrequency();
 
         if (ObjectUtils.isNotEmpty(cdm)) {
             DeliveryPjp dnfMap = deliveryPJPService.findByOutletCodeAndLoginIdAndMonthAndYear(cdm.getOutletcode(), cdm.getLoginid(), cdm.getMonth(), cdm.getYear());
@@ -34,41 +31,21 @@ public class PJPenrichment extends AbstractEnrichment<DeliveryPjp> {
                 return new OperationResult.StepResult(OperationResult.Status.OK);
             } else {
                 if (NullUtils.isNotNull(dnfMap.getDayAndFrequency())) {
-                    JsonNode jsonNode = null;
-                    try {
-                        jsonNode = objectMapper.readTree(dnfMap.getDayAndFrequency().data());
-                    } catch (JsonProcessingException e) {
-                        throw new RuntimeException(e);
-                    }
-                    if (!(jsonNode instanceof ArrayNode)) {
-                        throw new IllegalArgumentException("dayAndFrequency is not an array");
-                    }
-                    ArrayNode dnf = (ArrayNode) jsonNode;
-
+                    ArrayNode dnf = (ArrayNode) dnfMap.getDayAndFrequency();
                     Set<JsonNode> uniqueElements = new HashSet<>();
 
                     for (JsonNode element : dnf) {
                         uniqueElements.add(element);
                     }
 
-                    try {
-                        JsonNode dayandfreqNode = objectMapper.readTree(dayandfreq.data());
-                        if (!(dayandfreqNode instanceof ArrayNode)) {
-                            throw new IllegalArgumentException("dayAndFrequency (cdm) is not an array");
+                    for (JsonNode element : dayandfreq) {
+                        if (uniqueElements.contains(element)) {
+                            throw new IllegalArgumentException("dayandfreq already present in dnfMap");
                         }
-                        ArrayNode dayandfreqArray = (ArrayNode) dayandfreqNode;
-                        for (JsonNode element : dayandfreqArray) {
-                            if (uniqueElements.contains(element)) {
-                                throw new IllegalArgumentException("dayandfreq already present in dnfMap");
-                            }
-                        }
-                        dayandfreqArray.forEach(dnf::add);
-                        JsonNode finalPjp = objectMapper.convertValue(dnf, JsonNode.class);
-                        cdm.setDayAndFrequency(JSON.valueOf(finalPjp.toString()));
-                    } catch (JsonProcessingException e) {
-                        throw new RuntimeException(e);
                     }
-
+                    dayandfreq.forEach(dnf::add);
+                    JsonNode finalPjp = objectMapper.convertValue(dnf, JsonNode.class);
+                    cdm.setDayAndFrequency((ArrayNode) finalPjp);
                 }
             }
         }
