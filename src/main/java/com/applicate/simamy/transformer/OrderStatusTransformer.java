ccda1696df9d994e1f5589022a12db1677158efe
siddharth-2510 <siddharth.jaiswal@salescode.ai>
2025-11-21 12:38:28 +0530

    6 more

--- src/main/java/com/applicate/simamy/transformer/OrderStatusTransformer.java
+++ src/main/java/com/applicate/simamy/transformer/OrderStatusTransformer.java
@@ -1,91 +1,92 @@
-//package com.applicate.simamy.transformer;
-//
-//import com.applicate.services.channelkart.models.Order;
-//import com.applicate.services.channelkart.services.OrderService;
-//import com.applicate.services.channelkart.services.SpringContext;
-//import com.applicate.services.channelkart.transformers.AbstractTransformer;
-//import com.applicate.services.channelkart.utils.JSONUtils;
-//import com.fasterxml.jackson.databind.node.ArrayNode;
-//import com.fasterxml.jackson.databind.node.ObjectNode;
-//import org.apache.commons.lang3.ObjectUtils;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//
-//import java.util.Map;
-//
-//public class OrderStatusTransformer extends AbstractTransformer<Map<String, Object>, Map<String, Object>> {
-//
-//    OrderService orderService = SpringContext.getBean(OrderService.class);
-//
-//    private Logger logger = LoggerFactory.getLogger(OrderStatusTransformer.class);
-//
-//    @Override
-//    public Object transform(Map<String, Object> ObjectMap) {
-//        ArrayNode notebookMessage = JSONUtils.convert(ObjectMap.get("NotebookMessage"), ArrayNode.class);
-//        String orderNumber = notebookMessage.get(0).asText();
-//
-//        Order ord = orderService.findFullLoadedOrder(orderNumber);
-//        if (ObjectUtils.isEmpty(ord)) {
-//
-//            logger.error("found no order record for order number : {}", orderNumber);
-//            throw new com.applicate.services.channelkart.exceptions.TransformationException("Not a cokebuddy order status");
-//        } else {
-//
-//            if (ObjectMap.get("MainProcessingStatus") == null) {
-//
-//                String SubProcessingStatus1 = ObjectMap.get("SubProcessingStatus1").toString();
-//                if (ObjectUtils.isNotEmpty(ord)) {
-//                    switch (SubProcessingStatus1) {
-//                        case "UNP":
-//                        case "REJECTED":
-//                            ord.setStatus("REJECTED");
-//                            break;
-//                        case "HLD":
-//                        case "HOLD":
-//                            ord.setStatus("Hold");
-//                            break;
-//
-//                    }
-//                    ObjectNode extended = (ObjectNode) ord.getExtendedAttributes();
-//                    extended.put("TransactionNumber", String.valueOf(ObjectMap.get("TransactionNumber")));
-//                    extended.put("nr", "1");
-//                    ord.setExtendedAttributes(extended);
-//                    logger.info("data transformed successfully for : {}", orderNumber);
-//                    return ord;
-//                }
-//            }
-//
-//            String mainProcessingStatus = ObjectMap.get("MainProcessingStatus").toString();
-//
-//            switch (mainProcessingStatus) {
-//                case "OEB":
-//                case "CONFIRMED":
-//                    ord.setStatus("CONFIRMED");
-//                    break;
-//                case "INF":
-//                case "OUT FOR DELIVERY":
-//                    ord.setStatus("out for delivery");
-//                    break;
-//                case "SMF":
-//                case "DELIVERED":
-//                    ord.setStatus("Delivered");
-//                    break;
-//
-//            }
-//            ObjectNode extended = (ObjectNode) ord.getExtendedAttributes();
-//            extended.put("nr", "1");
-//            extended.put("TransactionNumber", String.valueOf(ObjectMap.get("TransactionNumber")));
-//            ord.setExtendedAttributes(extended);
-//            logger.info("data transformed successfully for : {}", orderNumber);
-//            return ord;
-//        }
-//    }
-//}
-//
-//
-//
-//
-//
-//
-//
-//
+package com.applicate.simamy.transformer;
+
+import com.applicate.services.channelkart.services.OrderService;
+import com.applicate.services.channelkart.services.ServiceLocator;
+import com.applicate.services.channelkart.utils.JSONUtils;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.salescode.dim.etl.transformation.AbstractTransformer;
+import com.salescode.dim.etl.transformation.service.DataTransformationService;
+import com.salescode.dim.jooq.generated.tables.pojos.Orders;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+public class OrderStatusTransformer extends AbstractTransformer<Map<String, Object>, Map<String, Object>> {
+
+
+    OrderService orderService = (OrderService) ServiceLocator.lookup(Orders.class);
+    private Logger logger = LoggerFactory.getLogger(OrderStatusTransformer.class);
+
+    @Override
+    public Map<String,Object> transform(Map<String, Object> ObjectMap) {
+        ArrayNode notebookMessage = JSONUtils.getObjectMapper().convertValue(ObjectMap.get("NotebookMessage"), ArrayNode.class);
+        String orderNumber = notebookMessage.get(0).asText();
+
+        Orders ord = orderService.findFullLoadedOrder(orderNumber);
+        if (ObjectUtils.isEmpty(ord)) {
+
+            logger.error("found no order record for order number : {}", orderNumber);
+            throw new DataTransformationService.TransformationException("Not a cokebuddy order status");
+        } else {
+
+            if (ObjectMap.get("MainProcessingStatus") == null) {
+
+                String SubProcessingStatus1 = ObjectMap.get("SubProcessingStatus1").toString();
+                if (ObjectUtils.isNotEmpty(ord)) {
+                    switch (SubProcessingStatus1) {
+                        case "UNP":
+                        case "REJECTED":
+                            ord.setStatus("REJECTED");
+                            break;
+                        case "HLD":
+                        case "HOLD":
+                            ord.setStatus("Hold");
+                            break;
+
+                    }
+                    ObjectNode extended = JSONUtils.getObjectMapper().convertValue(ord.getExtendedAttributes(), ObjectNode.class);
+                    extended.put("TransactionNumber", String.valueOf(ObjectMap.get("TransactionNumber")));
+                    extended.put("nr", "1");
+                    ord.setExtendedAttributes(JSONUtils.getObjectMapper().convertValue(extended, JsonNode.class));
+                    logger.info("data transformed successfully for : {}", orderNumber);
+                    return JSONUtils.getObjectMapper().convertValue(ord,Map.class);
+                }
+            }
+
+            String mainProcessingStatus = ObjectMap.get("MainProcessingStatus").toString();
+
+            switch (mainProcessingStatus) {
+                case "OEB":
+                case "CONFIRMED":
+                    ord.setStatus("CONFIRMED");
+                    break;
+                case "INF":
+                case "OUT FOR DELIVERY":
+                    ord.setStatus("out for delivery");
+                    break;
+                case "SMF":
+                case "DELIVERED":
+                    ord.setStatus("Delivered");
+                    break;
+
+            }
+            ObjectNode extended = JSONUtils.getObjectMapper().convertValue(ord.getExtendedAttributes(), ObjectNode.class);
+            extended.put("nr", "1");
+            extended.put("TransactionNumber", String.valueOf(ObjectMap.get("TransactionNumber")));
+            ord.setExtendedAttributes(JSONUtils.getObjectMapper().convertValue(extended, JsonNode.class));
+            return JSONUtils.getObjectMapper().convertValue(ord,Map.class);
+        }
+    }
+}
+
+
+
+
+
+
+
+
--- src/main/java/com/applicate/simamy/transformer/SimaInclusionTransformer.java
+++ src/main/java/com/applicate/simamy/transformer/SimaInclusionTransformer.java
@@ -1,105 +1,101 @@
-//package com.applicate.simamy.transformer;
-//
-//import com.applicate.services.channelkart.exceptions.TransformationException;
-//import com.applicate.services.channelkart.models.GenericEntity;
-//import com.applicate.services.channelkart.repository.GenericEntityRepository;
-//import com.applicate.services.channelkart.services.SpringContext;
-//import com.applicate.services.channelkart.transformers.AbstractTransformer;
-//import com.applicate.services.channelkart.utils.JSONUtils;
-//import com.applicate.services.channelkart.utils.ReflectionUtils;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fasterxml.jackson.databind.node.ObjectNode;
-//
-//import java.time.Duration;
-//import java.time.Instant;
-//import java.util.*;
-//import java.util.function.Supplier;
-//
-//
-//public class SimaInclusionTransformer extends AbstractTransformer<Map<String,Object>, Map<String,Object>> {
-//    static final String NAME = "SchemeInclusion";
-//    static final String TRO = "TRO";
-//    static final String OUTLET_CODE = "Outlet id";
-//    final GenericEntityRepository genericEntityRepository = SpringContext.getBean(GenericEntityRepository.class);
-//    public Object transform(Map<String, Object> inputMap) {
-//
-//        List<Object> dataList = JSONUtils.convert(inputMap.get("data_value"), List.class);
-//        if (dataList == null || dataList.isEmpty()) {
-//            throw new TransformationException("Data list is empty");
-//        }
-//
-//        String schemeId = JSONUtils.convert(dataList.get(0),Map.class).get(TRO).toString();
-//        try {
-//            Class<?> distributedCacheClass = Class.forName("com.applicate.services.channelkart.cache.DistributedCache");
-//            Object distributedCacheBean = SpringContext.getBean(distributedCacheClass);
-//            Supplier<GenericEntity> transformationSupplier = () -> {
-//                List<Map<String, Object>> filteredDataList = new ArrayList<>();
-//                for (Object data : dataList) {
-//                    Map<String, Object> input = JSONUtils.convert(data, Map.class);
-//                    String tro = input.get(TRO) != null ? input.get(TRO).toString() : null;
-//                    String outletCode = input.get(OUTLET_CODE) != null ? input.get(OUTLET_CODE).toString() : null;
-//                    if (tro != null && !tro.isEmpty() && outletCode != null && !outletCode.isEmpty()) {
-//                        filteredDataList.add(input);
-//                    }
-//                }
-//                if (filteredDataList.isEmpty()) {
-//                    throw new TransformationException("TRO or Outlet code not found");
-//                }
-//                String schemeIdToDelete = filteredDataList.get(0).get(TRO).toString();
-//
-//                deleteInBatch(schemeIdToDelete);
-//                Map<String, Object> genericEntity = new HashMap<>();
-//                genericEntity.put("name", NAME);
-//                genericEntity.put("key1", schemeIdToDelete);
-//                genericEntity.put("id", schemeIdToDelete.concat("-" + NAME));
-//                ObjectNode payload = new ObjectMapper().createObjectNode();
-//                for (Object data : filteredDataList) {
-//                    Map<String, Object> input = JSONUtils.convert(data, Map.class);
-//                    String outlet = input.get(OUTLET_CODE).toString();
-//                    payload.put(outlet, outlet);
-//                }
-//                genericEntity.put("payload", payload);
-//                return JSONUtils.convert(genericEntity,GenericEntity.class);
-//            };
-//
-//            // Execute transformation with distributed lock
-//            return ReflectionUtils.invokeMethod(distributedCacheBean, "withLock", schemeId, transformationSupplier);
-//
-//        }
-//        catch (Exception e) {
-//            throw new TransformationException("Error during transformation: " + e.getMessage(), e);
-//        }
-//    }
-//
-//    private void deleteInBatch(String schemeId) {
-//        List<GenericEntity> entities = genericEntityRepository.findByName(NAME);
-//        if (!entities.isEmpty()) {
-//            // Find the maximum lastModifiedTime among all entities
-//            Date maxLastModifiedTime = entities.stream()
-//                    .filter(e -> e.getLastModifiedTime() != null)
-//                    .map(GenericEntity::getLastModifiedTime)
-//                    .max(Date::compareTo)
-//                    .orElse(null);
-//
-//            // Only delete if the max last modified time is older than 30 minutes
-//            if (maxLastModifiedTime != null && isOlderThan30Minutes(maxLastModifiedTime)) {
-//                genericEntityRepository.deleteAllInBatch(entities);
-//            }
-//            else{
-//                entities = genericEntityRepository.findByNameAndKey1(NAME,schemeId);
-//                genericEntityRepository.deleteAllInBatch(entities);
-//            }
-//        }
-//
-//    }
-//    private boolean isOlderThan30Minutes(Date lastModifiedTime) {
-//        if (lastModifiedTime == null) {
-//            return false;
-//        }
-//
-//        Instant lastModifiedInstant = lastModifiedTime.toInstant();
-//        Instant thirtyMinutesAgo = Instant.now().minus(Duration.ofMinutes(30));
-//
-//        return lastModifiedInstant.isBefore(thirtyMinutesAgo);
-//    }
-//}
+package com.applicate.simamy.transformer;
+
+import com.applicate.services.channelkart.services.GenericEntityService;
+import com.applicate.services.channelkart.services.ServiceLocator;
+import com.applicate.services.channelkart.utils.JSONUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.salescode.dim.etl.transformation.AbstractTransformer;
+import com.salescode.dim.etl.transformation.service.DataTransformationService;
+import com.salescode.dim.jooq.impl.GenericEntity;
+import com.salescode.dim.utils.ReflectionUtils;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.function.Supplier;
+
+
+public class SimaInclusionTransformer extends AbstractTransformer<Map<String,Object>, Map<String,Object>> {
+    static final String NAME = "SchemeInclusion";
+    static final String TRO = "TRO";
+    static final String OUTLET_CODE = "Outlet id";
+    final GenericEntityService genericEntityRepository = (GenericEntityService) ServiceLocator.lookup(GenericEntity.class);
+    public Map<String,Object> transform(Map<String, Object> inputMap) {
+
+        List<Object> dataList = JSONUtils.getObjectMapper().convertValue(inputMap.get("data_value"), List.class);
+        if (dataList == null || dataList.isEmpty()) {
+            throw new DataTransformationService.TransformationException("Data list is empty");
+        }
+
+        String schemeId = JSONUtils.getObjectMapper().convertValue(dataList.get(0),Map.class).get(TRO).toString();
+        try {
+            Supplier<GenericEntity> transformationSupplier = () -> {
+                List<Map<String, Object>> filteredDataList = new ArrayList<>();
+                for (Object data : dataList) {
+                    Map<String, Object> input = JSONUtils.getObjectMapper().convertValue(data, Map.class);
+                    String tro = input.get(TRO) != null ? input.get(TRO).toString() : null;
+                    String outletCode = input.get(OUTLET_CODE) != null ? input.get(OUTLET_CODE).toString() : null;
+                    if (tro != null && !tro.isEmpty() && outletCode != null && !outletCode.isEmpty()) {
+                        filteredDataList.add(input);
+                    }
+                }
+                if (filteredDataList.isEmpty()) {
+                    throw new DataTransformationService.TransformationException("TRO or Outlet code not found");
+                }
+                String schemeIdToDelete = filteredDataList.get(0).get(TRO).toString();
+
+                deleteInBatch(schemeIdToDelete);
+                Map<String, Object> genericEntity = new HashMap<>();
+                genericEntity.put("name", NAME);
+                genericEntity.put("key1", schemeIdToDelete);
+                genericEntity.put("id", schemeIdToDelete.concat("-" + NAME));
+                ObjectNode payload = new ObjectMapper().createObjectNode();
+                for (Object data : filteredDataList) {
+                    Map<String, Object> input = JSONUtils.getObjectMapper().convertValue(data, Map.class);
+                    String outlet = input.get(OUTLET_CODE).toString();
+                    payload.put(outlet, outlet);
+                }
+                genericEntity.put("payload", payload);
+                return JSONUtils.getObjectMapper().convertValue(genericEntity, GenericEntity.class);
+            };
+
+            // Execute transformation with distributed lock
+            return ReflectionUtils.invokeMethod("withLock", schemeId, transformationSupplier);
+
+        }
+        catch (Exception e) {
+            throw new DataTransformationService.TransformationException("Error during transformation: " + e.getMessage(), e);
+        }
+    }
+
+    private void deleteInBatch(String schemeId) {
+        List<GenericEntity> entities = genericEntityRepository.findByName(NAME);
+        if (!entities.isEmpty()) {
+            LocalDateTime maxLastModifiedTime = entities.stream()
+                    .map(e -> e.getLastModifiedTime())
+                    .filter(Objects::nonNull)
+                    .max(LocalDateTime::compareTo)
+                    .orElse(null);
+
+            // Only delete if the max last modified time is older than 30 minutes
+            if (maxLastModifiedTime != null && isOlderThan30Minutes(maxLastModifiedTime)) {
+                genericEntityRepository.deleteInBatch(entities);
+            }
+            else{
+                entities = genericEntityRepository.findByNameAndKeys(NAME,schemeId);
+                genericEntityRepository.deleteInBatch(entities);
+            }
+        }
+
+    }
+    private boolean isOlderThan30Minutes(LocalDateTime lastModifiedTime) {
+        if (lastModifiedTime == null) {
+            return false;
+        }
+        LocalDateTime thirtyMinutesAgo = LocalDateTime.now().minus(Duration.ofMinutes(30));
+        return lastModifiedTime.isBefore(thirtyMinutesAgo);
+    }
+}
